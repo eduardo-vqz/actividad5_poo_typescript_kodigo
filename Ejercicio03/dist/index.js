832bd202beeb33e82a87115bae55d6bd0b642993
Eduardo Vásquez <eduardo.vqz968@gmail.com>
2025-08-06 22:43:06 -0600

    Ejercicio 3 fin

--- Ejercicio03/dist/index.js
+++ Ejercicio03/dist/index.js
@@ -1,34 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class Cancion {
-    public titulo: string;
-    public genero: string;
-    private autor: string = "";
-
     // Constructor que recibe título y género
-    constructor(titulo: string, genero: string) {
+    constructor(titulo, genero) {
+        this.autor = "";
         this.titulo = titulo;
         this.genero = genero;
     }
-
     // Setter para el autor
-    public setAutor(nombre: string): void {
+    setAutor(nombre) {
         this.autor = nombre;
     }
-
     // Getter para el autor
-    public getAutor(): string {
+    getAutor() {
         return this.autor;
     }
-
     // Método para mostrar todos los datos de la canción
-    public mostrarCancion(): void {
+    mostrarCancion() {
         console.log("Información de la Canción");
         console.log(`Título: ${this.titulo}`);
         console.log(`Género: ${this.genero}`);
         console.log(`Autor: ${this.getAutor()}`);
     }
 }
-
 // Implementación de la clase
 const cancion1 = new Cancion("Bohemian Rhapsody", "Rock");
 cancion1.setAutor("Freddie Mercury");
 cancion1.mostrarCancion();
+//# sourceMappingURL=index.js.map
